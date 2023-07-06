8d21d57e015d9d750ac678ac3a5c532240d18fa4
roy <garam103@naver.com>
2023-07-06 18:41:58 +0900

    당근마켓 한 페이지 따라하기_실패

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,35 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(leading: Icon(Icons.menu),title: Text("제목입니다"),
-            actions: const [Icon(Icons.star), Icon(Icons.star)],),
-            body: SizedBox(
-                  child: IconButton(
-                    icon: Icon(Icons.star),
-                    onPressed: (){},
+            appBar: AppBar(title: Text("금호동3가", style: TextStyle(color: Colors.black),),
+              backgroundColor: Colors.white,
+              actions: <Widget>[
+                IconButton(onPressed: () => {}, icon: Icon(Icons.search, color: Colors.black,)),
+                IconButton(onPressed: () => {}, icon: Icon(Icons.menu, color: Colors.black,)),
+                IconButton(onPressed: () => {}, icon: Icon(Icons.add_alert, color: Colors.black,))
+              ]
+            ),
+            body: Container(
+              alignment: Alignment.center,
+              child: Row(
+                children: [
+                Image.asset('camera.jpg', width: 100, height: 200,alignment: Alignment.topLeft, ),
+                  Row(
+                    children: const [
+                      Column(
+                        crossAxisAlignment:CrossAxisAlignment.start,
+                          children: [
+                            Text("캐논 DSLR 100D (단렌즈,충전기 16기가SD 포함", style: TextStyle(fontSize: 19),
+                            softWrap: true,),
+                            Text("성동구 행당동 . 끌올 10분 전", style: TextStyle(color: Colors.black54)),
+                            Text("210,000원"),
+                            Text("♡4", textAlign: TextAlign.right),
+                          ],
+                      ),
+                    ],
                   )
+          ]
+              ),
             ),
 
             bottomNavigationBar: BottomAppBar(
