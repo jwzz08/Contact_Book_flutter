0150bc38f48e23a2c65d8bdd002084639939cb1b
roy <garam103@naver.com>
2023-07-12 16:20:49 +0900

    making a custom widget

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # contact
 
-===================================================
+
+Introduction
 
 My First Project by Flutter.
 Application of IOS, Android are made by Flutter.
@@ -9,5 +10,4 @@ Application of IOS, Android are made by Flutter.
 
 TODAY's REVIEW
 
-The reason why last homework was failed is I didn't give a "WIDTH" option to the code. So the picture and the letter were not aligned in one line.
-Also I didn't know how a text is aligned on the right in the Column, Container. A good way I solve is using "ROW" instead of "TEXT".
+Making a Custom Widget
--- lib/main.dart
+++ lib/main.dart
@@ -11,67 +11,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-              title: Text("금호동3가", style: TextStyle(color: Colors.black),),
-              backgroundColor: Colors.white,
-              actions: <Widget>[
-                IconButton(onPressed: () => {},
-                    icon: Icon(Icons.search, color: Colors.black,)),
-                IconButton(onPressed: () => {},
-                    icon: Icon(Icons.menu, color: Colors.black,)),
-                IconButton(onPressed: () => {},
-                    icon: Icon(Icons.add_alert, color: Colors.black,))
-              ]
-          ),
-          body: Container(
-            height: 150,
-            padding: EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Image.asset('camera.jpg', width: 100,
-                  height: 200,
-                  alignment: Alignment.topLeft,),
-                Container(child: Container(
-                  width: 300,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("캐논 DSLR 100D 단렌즈", style: TextStyle(fontSize: 18)),
-                      Text("성동구 행당동 . 끌올 10분 전",
-                          style: TextStyle(color: Colors.black54)),
-                      Text("210,000원"),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(Icons.favorite),
-                          Text("4"),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                )
-              ],
-            ),
-          ),
-
-
-          bottomNavigationBar: BottomAppBar(
-              child: SizedBox(
-                height: 50,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: const [
-                      Icon(Icons.phone),
-                      Icon(Icons.message),
-                      Icon(Icons.contact_page),
-                    ]
-                ),
-              )
-          ),
+          appBar: AppBar(),
+          body: ShopItem(),
         )
     );
   }
 }
 
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        child: Text("안녕")
+    );
+  }
+}
