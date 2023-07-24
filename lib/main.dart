5188b3bf183e356e98477e0debfe5cdbde8750cc
roy <garam103@naver.com>
2023-07-24 18:03:55 +0900

    Using a StatefulWidget and making a button

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
+
+class MyApp extends StatefulWidget {            //StatelessWidget랑 차이를 알아둘 것
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {        //state는 렌더링 해줌, 자주 바뀌는 데이터들한테 좋음
+  var a = 1;
+  var name = ['서미누', '유졸리', '유가람'];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(),
-          bottomNavigationBar: BottomLayout(),
-          body: ListView(
-            children: [
-              ListTile(
-                leading: Image(image: AssetImage("profile_icon.png"),height: 40, ),
-                title: Text("홍길동", style: TextStyle(fontSize: 20)),
-              ),
-              ListTile(
-                leading: Image(image: AssetImage("profile_icon.png"),height: 40, ),
-                title: Text("홍길동", style: TextStyle(fontSize: 20)),
-              ),
-              ListTile(
-                leading: Image(image: AssetImage("profile_icon.png"),height: 40, ),
-                title: Text("홍길동", style: TextStyle(fontSize: 20)),
-              )
-            ],
+          floatingActionButton: FloatingActionButton(
+            child: Text(a.toString()),
+            onPressed:() {
+              setState(() {               //state 사용할 거면 꼭 setState 써줘야함
+                a++;
+              });
+            },
           ),
+          appBar: AppBar(title: Text('Contact App'),),
+          bottomNavigationBar: BottomLayout(),
+          body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, i){
+              return ListTile(
+                leading: Image(image: AssetImage("profile_icon.png")),
+                title: Text(name[i]),
+              );
+            },
+          )
         )
     );
   }
